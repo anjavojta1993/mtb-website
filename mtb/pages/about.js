ab0e61a08c889c9a31374f46c3197fcc1dfa0e76
anjavojta1993 <anja.vojta@gmail.com>
2021-09-01 16:56:05 +0200

    Add portrait and change about page

--- mtb/pages/about.js
+++ mtb/pages/about.js
@@ -1,7 +1,9 @@
 import { css } from '@emotion/react';
 import Head from 'next/head';
+import Image from 'next/image';
 import Layout from '../components/Layout';
-import { h1, lightGrey, mediumText } from '../styles/sharedStyles';
+import Portrait from '../public/images/portrait.png';
+import { darkBlue, h1, lightGrey, mediumText } from '../styles/sharedStyles';
 
 const pageContainer = css`
   display: flex;
@@ -9,6 +11,50 @@ const pageContainer = css`
   width: 100%;
   align-items: center;
   margin-top: 40px;
+`;
+
+const leftContainer = css`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  justify-content: center;
+  height: 80vh;
+  align-items: center;
+  background-color: green;
+  margin-bottom: 50px;
+`;
+
+const leftContainerHeading = css`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  height: 10vh;
+  align-items: center;
+  font-size: ${mediumText};
+  font-weight: 700;
+  background-color: purple;
+`;
+
+const rightContainer = css`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  width: 50%;
+  padding: 10px;
+  height: 80vh;
+  align-items: center;
+  margin-bottom: 50px;
+  background-color: yellow;
+`;
+
+const testimonialContainer = css`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  justify-content: center;
+  height: 80vh;
+  align-items: center;
+  background-color: ${darkBlue};
 `;
 
 const heroContainer = css`
@@ -70,7 +116,38 @@ export default function About() {
           eiusmod tempor incididunt.”
         </div>
       </div>
-      <section css={pageContainer}>ICH BIN EIN TEXT</section>
+      <section css={pageContainer}>
+        <div css={leftContainer}>
+          <div css={leftContainerHeading}>MAG. MARLIES THERES BRUNNER, MTD</div>
+          <Image
+            src={Portrait}
+            width="500"
+            height="500"
+            alt="Portrait von Marlies Theres Brunner"
+          ></Image>
+        </div>
+        <div css={rightContainer}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum." fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+          mollit anim id est laborum." fugiat nulla pariatur. Excepteur sint
+          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+          mollit anim id .
+        </div>
+      </section>
+      <div css={testimonialContainer}>TESTIMONIALS</div>
     </Layout>
   );
 }
