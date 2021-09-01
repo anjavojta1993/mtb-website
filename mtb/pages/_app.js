0dea07c508b7926303d528a33275d4e7a6f71c3e
anjavojta1993 <anja.vojta@gmail.com>
2021-09-01 10:16:44 +0200

    Add global styles

--- mtb/pages/_app.js
+++ mtb/pages/_app.js
@@ -1,7 +1,32 @@
-import '../styles/globals.css';
+import { css, Global } from '@emotion/react';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Global
+        styles={css`
+          *,
+          *::after,
+          *::before {
+            box-sizing: border-box;
+          }
+          body {
+            margin: 0;
+            font-family: 'Spartan', sans-serif;
+            color: black;
+          }
+          a {
+            :hover {
+              border-bottom: 1px solid black;
+            }
+          }
+        `}
+      />
+      <Head />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
