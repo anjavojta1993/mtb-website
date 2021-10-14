93f38baafa5773db0e0dd838e01ddf2a94e7a37f
anjavojta1993 <anja.vojta@gmail.com>
2021-10-14 17:01:05 +0200

    Update email to marlies email address

--- mtb/pages/api/mail.js
+++ mtb/pages/api/mail.js
@@ -14,7 +14,7 @@ export default async function sendMail(req, res) {
 
   await mail
     .send({
-      to: 'anja.vojta@gmail.com',
+      to: 'marlies.brunner@aon.at',
       from: 'info@marliestheresbrunner.at',
       subject: 'Neue Nachricht Systemischer Coach!',
       text: message,
