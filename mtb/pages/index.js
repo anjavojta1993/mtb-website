ac366e73855bf7178c8b18f1777aee7e4704e6a2
anjavojta1993 <anja.vojta@gmail.com>
2021-10-22 12:21:33 +0200

    Add https reirect heroku

--- mtb/pages/index.js
+++ mtb/pages/index.js
@@ -14,3 +14,19 @@ export default function Home() {
     </>
   );
 }
+
+export async function getServerSideProps() {
+  // Redirect from HTTP to HTTPS on Heroku
+  if (
+    context.req.headers.host &&
+    context.req.headers['x-forwarded-proto'] &&
+    context.req.headers['x-forwarded-proto'] !== 'https'
+  ) {
+    return {
+      redirect: {
+        destination: `https://${context.req.headers.host}/index`,
+        permanent: true,
+      },
+    };
+  }
+}
