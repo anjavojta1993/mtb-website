0b1781f6cfdfe827532843a015167915d5e38f4e
anjavojta1993 <anja.vojta@gmail.com>
2023-09-20 11:38:15 +0200

    Update meta tags for FB and linkedin

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { css, Global } from '@emotion/react';
 import Head from 'next/head';
+import DisplayPhoto from '../public/images/title_photo_blue.png';
 
 const contentWrapper = css`
   padding-bottom: 2.5rem;
@@ -29,6 +30,13 @@ function MyApp({ Component, pageProps }) {
         ></meta>
         <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1"></meta>
         <meta name="HandheldFriendly" content="true"></meta>
+        <meta property="og:title" content="Business- und Karrierecoaching" />
+        <meta property="og:image" content={DisplayPhoto} />
+        <meta
+          property="og:description"
+          content="Einstieg - Aufstieg - Orientierung - Veränderung - Neustart. Beratung und Begleitung in jeder Phase Ihres Berufslebens!"
+        />
+        <meta property="og:url" content="https://marliestheresbrunner.at" />
       </Head>
       <Component css={contentWrapper} {...pageProps} />
     </>
--- pages/index.js
+++ pages/index.js
@@ -8,17 +8,6 @@ export default function Home() {
       <Layout>
         <Head>
           <title>Home</title>
-          <meta property="og:title" content="Marlies Theres Brunner" />
-          <meta
-            property="og:image"
-            content="/public/images/title_photo_blue.png"
-          />
-          <meta
-            property="og:description"
-            content="Business- und Karrierecoaching. Beratung und Begleitung in jeder Phase
-            Ihres Berufslebens!"
-          />
-          <meta property="og:url" content="//www.marliestheresbrunner.at" />
         </Head>
         <Hero />
       </Layout>
