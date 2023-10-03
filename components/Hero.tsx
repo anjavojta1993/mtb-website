614673b277cd3fa82c36c739c09a6dc813b904a1
anjavojta1993 <anja.vojta@gmail.com>
2023-10-03 11:09:47 +0200

    Change meta tags

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -245,27 +245,6 @@ export default function Hero() {
     <>
       <Head>
         <title>Business- und Karrierecoaching</title>
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:title"
-          content="Business- und Karrierecoaching"
-          key="ogtitle"
-        />
-        <meta
-          property="og:image"
-          content="../public/images/title_photo_blue.png"
-          key="ogimage"
-        />
-        <meta
-          property="og:description"
-          content="Beratung und Begleitung in jeder Phase Ihres Berufslebens!"
-          key="ogdesc"
-        />
-        <meta
-          property="og:url"
-          content="https://marliestheresbrunner.at"
-          key="ogurl"
-        />
       </Head>
       <div css={pageContainer}>
         <div css={heroContainer}>
--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,17 @@ export default function Home() {
     <>
       <Layout>
         <Head>
-          <title>Home</title>
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Business- und Karrierecoaching" />
+          <meta
+            property="og:description"
+            content="Beratung und Begleitung in jeder Phase Ihres Berufslebens!"
+          />
+          <meta
+            property="og:image"
+            content="../public/images/title_photo_blue.png"
+          />
+          <meta property="og:url" content="https://marliestheresbrunner.at" />
         </Head>
         <Hero />
       </Layout>
