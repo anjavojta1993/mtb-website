0d0b4988d4161b4804b68e5ba0ec47aea6db392a
anjavojta1993 <anja.vojta@gmail.com>
2023-11-16 15:36:55 +0100

    Add google metatags

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,14 @@ import Layout from '../components/Layout';
 export default function Home() {
   return (
     <>
+      <Head>
+        <meta
+          name="Business - und Karrierecoaching"
+          content="Beratung und Begleitung in jeder Phase Ihres Berufslebens!
+          Einstieg – Aufstieg – Orientierung – Veränderung – Neustart"
+          key="desc"
+        />
+      </Head>
       <Layout>
         <Hero />
       </Layout>
